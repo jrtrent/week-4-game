a0916f79bdf50b35073dbde862d9f5b3757e98c8
jrtrent <jessrtrent@gmail.com>
2018-03-15 01:37:49 -0400

    w4

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # week-4-game
+You will be given a random nuamber at the start of the game
+There are four crystals belw. By clicking on a crystal you will add a specific amount of points to your total score. You win the game by matching your total score to the random number, you lose the game if your total score goes above the random number.
+Each time the game starts to game will change the values of each crystal.
+
+To get started click on any crytal.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,74 @@
 $(document).ready(function(){
-    var wins= 0;
-    var loses= 0;
-    var userscore = ' ';
+    var displaynum = Math.floor(Math.random() * 120 + 19 );
+    $("#random").text(displaynum);
     var greencrystal =(Math.floor(Math.random() * 12 + 1));  
     var bluecrystal = (Math.floor(Math.random() * 12 + 1));
     var purplecrystal = (Math.floor(Math.random() * 12 + 1));
     var redcrystal = (Math.floor(Math.random() * 12 + 1));
-    var firstValue = ' ';
-    var secondvalue = ' ';
-    var thirdvalue = ' ';
-    var fourthvalue = ' ';
-    var displaynum = Math.floor(Math.random() * 120 + 19 );
-
-
     
-    $("#random").text(displaynum);
+    var wins= 0;
+    var losses= 0;
+    var userscore = 0;
+   
+    function winlose() {
+        if (userscore === displaynum) {
+            wins++;
+        } else if (userscore > displaynum) {
+            losses++;
+        }
+        $('#Wins').text(wins);
+        $('#Losses').text(losses);
+    }
+    
+
     $("#crystals").on('click',"#green", function () {
-            var value = $(this).val();
+            userscore =userscore + greencrystal;
+            console.log(greencrystal);
+        $('#score').text(userscore);
+            if (userscore === displaynum) {
+                wins++;
+            } else if (userscore > displaynum) {
+                losses++
+            }
+        })
+    
         
     $("#crystals").on('click',"#blue", function () {
-        parseInt(bluecrystal)
-            var value = $(this).val();
-        });
-    $("#crystals").on("click", '#purple', function (){
-       
-            });
-    $("#crystals").on("click", "#red" , function () {
-       
-            console.log(redcrystal);
-        });
-    $(".crvalue").click(function () {
-    
-        $(".crvalue").each(function () {
-                userscore += Number($(".crvalue").val()); 
-                console.log(userscore);     
+        userscore =userscore + bluecrystal;
+        console.log(bluecrystal);
+        $('#score').text(userscore);
+            if (userscore === displaynum) {
+                wins++;
+            } else if (userscore > displaynum) {
+                losses++
+            }
         })
-        
+    $("#crystals").on("click", '#purple', function (){
+        userscore =userscore + purplecrystal;
+        console.log(purplecrystal);
+        $('#score').text(userscore);
+        console.log(purplecrystal);
+            if (userscore === displaynum) {
+                wins++;
+            } else if (userscore > displaynum) {
+                losses++
+            }
+        })
+    $("#crystals").on("click", "#red" , function () {
+        userscore =userscore + redcrystal;
+        console.log(redcrystal);
+        $('#score').text(userscore);
+        })
+
+    
     });
     
     
-     if (i>= 10) {
-        document.getElementById("#game").reset()};
+     //if (i>= 10) {
+       // document.getElementById("#game").reset()};
 
     
-})
+
 
 
 
